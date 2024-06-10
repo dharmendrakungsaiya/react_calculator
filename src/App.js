6f9ecc450a84aee54fbb9c8bce3d80a19683b575
Dharmendra Kungsaiya <dharmendra.kungsaiya20@gmail.com>
2024-06-10 22:20:09 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState('');
+  const [calculatedResult, setCalculatedResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result + e.target.value);
+  }
+  
+  const clear = () => {
+    setResult("");
+    setCalculatedResult("");
+  }
+
+  const calculate = () => {
+    if (result === ''){
+      setCalculatedResult("Error")
+    } else {
+      setCalculatedResult(eval(result));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>React Calculator</h1>
+      
+      <form>
+        <input type='text' value={result} readOnly/>
+      </form>
+      <p>{calculatedResult}</p>
+      <div className='keypad'>
+        <button onClick={handleClick} value='7'>7</button>
+        <button onClick={handleClick} value='8'>8</button>
+        <button onClick={handleClick} value='9'>9</button>
+        <button onClick={handleClick} value='+'>+</button>
+        <button onClick={handleClick} value='4'>4</button>
+        <button onClick={handleClick} value='5'>5</button>
+        <button onClick={handleClick} value='6'>6</button>
+        <button onClick={handleClick} value='-'>-</button>
+        <button onClick={handleClick} value='1'>1</button>
+        <button onClick={handleClick} value='2'>2</button>
+        <button onClick={handleClick} value='3'>3</button>
+        <button onClick={handleClick} value='*'>*</button>
+        <button onClick={clear} value='C'>C</button>
+        <button onClick={handleClick} value='0'>0</button>
+        <button onClick={calculate} value='='>=</button>
+        <button onClick={handleClick} value='/'>/</button>
+      </div>
     </div>
   );
 }
